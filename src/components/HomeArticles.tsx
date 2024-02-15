6d6bc7787ecca936eaabb96ea5a39dd0a7fbc914
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-02-15 17:06:49 +0100

    da finire routes / link articoli

--- src/components/HomeArticles.tsx
+++ src/components/HomeArticles.tsx
@@ -28,27 +28,25 @@ const HomeArticles = () => {
  }, []);
 
  return (
-    <Container className="my-5">
-        <h2 className="text-white text-center mb-5">Space Articles</h2>
-      <Row className="g-3">
-        {articles && articles.results.map((article, i) => (
-          <Col className="col col-xs-6 col-md-3 col-lg-4" key={i}>
-           <Card className="h-100">
-      <Card.Img variant="top" src={article.image_url}/>
-      <Card.Body>
-        <Card.Title>{article.title}</Card.Title>
-        <Card.Text>
-         {article.summary}
-        </Card.Text>
-        <Button variant="primary">Go to full Article</Button>
-      </Card.Body>
-    </Card>
-        
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
+  <Container className="my-5">
+   <h2 className="text-white text-center mb-5">Space Articles</h2>
+   <Row className="g-3">
+    {articles &&
+     articles.results.map((article, i) => (
+      <Col className=" col col-xs-6 col-md-3 col-lg-4" key={i}>
+       <Card className="h-100 card shadow text-white">
+        <Card.Img variant="top" src={article.image_url} />
+        <Card.Body>
+         <Card.Title>{article.title}</Card.Title>
+         <Card.Text>{article.summary}</Card.Text>
+         <Button variant="dark">Go to full Article</Button>
+        </Card.Body>
+       </Card>
+      </Col>
+     ))}
+   </Row>
+  </Container>
+ );
 };
 
 export default HomeArticles;
